a13305be44ff869df43c42c6b6a7da8a1e1722c3
lewi-g <dr.lewi@gmail.com>
2020-04-06 16:28:22 -0400

    Updates

---
+++ precache-manifest.a838b9ba9a107f84e869c482aade4e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeeb9ec3ab41812d026b875c6c1f6d26",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "d5db9e6fcba3a649a3f2",
+    "url": "/movie-search/static/css/main.0a477730.chunk.css"
+  },
+  {
+    "revision": "2343cd9c1b51778ea5df",
+    "url": "/movie-search/static/js/2.cb0c2ba0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.cb0c2ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5db9e6fcba3a649a3f2",
+    "url": "/movie-search/static/js/main.63061c3f.chunk.js"
+  },
+  {
+    "revision": "08d1cf6968dfcbb7f688",
+    "url": "/movie-search/static/js/runtime-main.fd4c7407.js"
+  }
+]);
